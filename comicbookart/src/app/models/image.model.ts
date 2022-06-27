3d24135db6dc101cb05846cd6b5b50e7a07224f7
Jordan <jbrockerrudow@gmail.com>
2022-06-27 09:49:23 -0500

    Bug Fixes - Model Classes

--- comicbookart/src/app/models/image.model.ts
+++ comicbookart/src/app/models/image.model.ts
@@ -1,6 +1,7 @@
+import { Artist } from './artist.model';
 
 export class Image {
   id: string;
-  image_path: string
-  artist: [Artist]
+  image_path: string;
+  artist: [Artist];
 }
--- comicbookart/src/app/models/post.model.ts
+++ comicbookart/src/app/models/post.model.ts
@@ -1,9 +1,11 @@
+import { Image } from './image.model';
+import { Category } from './category.model';
+
 export class Post {
   id: string;
   title: string;
   caption: string;
   author: string;
-  attachment: [Image],
-  categories: [Category]
-
+  attachment: [Image];
+  category: [Category];
 }
