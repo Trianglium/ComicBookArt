75c022943bc978e43ad808fb7887c66c6afa7e7e
Jordan <jbrockerrudow@gmail.com>
2022-07-05 08:39:13 -0500

    WIP Bug Fixes - - error TS2307

--- comicbookart/src/app/pages/gallery/gallery.component.ts
+++ comicbookart/src/app/pages/gallery/gallery.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IMAGES } from '.../shared/images';
 import { Image } from '.../models/image.model';
 
 @Component({
@@ -8,7 +7,33 @@ import { Image } from '.../models/image.model';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  images: IMAGES;
+  images: Image[] = [
+    {image_path: 'comics/0.png'},
+    {image_path: 'comics/1.png'},
+    {image_path: 'comics/2.png'},
+    {image_path: 'comics/3.png'},
+    {image_path: 'comics/4.png'},
+    {image_path: 'comics/5.png'},
+    {image_path: 'comics/6.png'},
+    {image_path: 'comics/7.png'},
+    {image_path: 'comics/8.png'},
+    {image_path: 'comics/9.png'},
+    {image_path: 'comics/10.png'},
+    {image_path: 'comics/11.png'},
+    {image_path: 'comics/12.png'},
+    {image_path: 'comics/13.png'},
+    {image_path: 'comics/14.png'},
+    {image_path: 'comics/15.png'},
+    {image_path: 'comics/16.png'},
+    {image_path: 'comics/17.png'},
+    {image_path: 'comics/18.png'},
+    {image_path: 'comics/19.png'},
+    {image_path: 'comics/20.png'},
+    {image_path: 'comics/21.png'},
+    {image_path: 'comics/22.png'},
+    {image_path: 'comics/23.png'}
+  ];
+  
 
   constructor() { }
 
