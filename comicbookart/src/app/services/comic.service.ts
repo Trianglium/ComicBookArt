9fa979bd6c3d8bdf21979d238886be7a5bf1b2a5
Jordan <jbrockerrudow@gmail.com>
2022-07-09 11:26:26 -0500

    Create Comic Services

--- comicbookart/src/app/services/comic.service.ts
+++ comicbookart/src/app/services/comic.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Image } from '../shared/image';
+import { IMAGES } from '../shared/images';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,12 @@ import { Injectable } from '@angular/core';
 export class ComicService {
 
   constructor() { }
+
+  getComics(): Image[] {
+    return IMAGES;
+  }
+
+  getComic(name: string): Image[] {
+    return IMAGES.filter((image) => image.name === name)[0];
+  }
 }
